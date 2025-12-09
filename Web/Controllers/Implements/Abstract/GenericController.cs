e77741d8634cf14637ccccc3e75b9ec287e34e8f
JesusCarvajal017 <jesusfernandocarvajal2018@gmail.com>
2025-12-09 04:53:04 -0500

    fix: builder aut

--- Web/Controllers/Implements/Abstract/GenericController.cs
+++ Web/Controllers/Implements/Abstract/GenericController.cs
@@ -11,7 +11,7 @@ namespace Web.Controllers.Implements.Abstract
 {
     [ApiController]
     [Route("api/[controller]")]
-    //[Authorize]
+    [Authorize]
     public abstract class GenericController<
             TEntity,
             TReadDto,
